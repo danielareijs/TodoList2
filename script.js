4faae3ecf8173fe183c05803849bb89e11fb551d
daniela <danielareijs@gmail.com>
2020-11-16 21:28:33 +0100

    removeBtn, markComplete, markActive

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ var getAndDisplayAllTasks = function(){
     success: function(response, textStatus){
       $('#todo-list').empty();
       response.tasks.forEach(function(item){
-        console.log(item.content)
-        $('#todo-list').append('<p>' + item.content + '</p><button>Remove</button>');
-      })
+        $('#todo-list').append('<div class="row"><p class="col-xs-8">' + item.content + '</p><button class="removeBtn" data-id="' + item.id + '">Delete</button><input type="checkbox" class="markComplete" data-id="' + item.id + '"' + (item.completed ? 'checked' : '') + '>');
+      });
     },
     error: function(request, textStatus, errorMessage){
       console.log(errorMessage);
@@ -40,12 +39,66 @@ $.ajax({
 })
 };
 
-
- $('#createTask').on('submit', function(e){
+$('#createTask').on('submit', function(e){
    e.preventDefault();
-  addNewTask();
+   addNewTask();
 })
 
+ var deleteTask = function(id){
+  $.ajax({
+    type: 'DELETE',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=217',
+    success: function(response, textStatus){
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage){
+      console.log(errorMessage);
+    }
+  })
+};
+
+$(document).on('click', '.removeBtn', function(){
+  deleteTask($(this).data('id'))
+});
+
+
+var markTaskAsComplete = function(id){
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=217',
+    dataType: 'json',
+    success: function(response, textStatus){
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage){
+      console.log(errorMessage);
+    }
+  })
+};
+
+
+var markTastAsActive = function(){
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=217',
+    dataType: 'json',
+    success: function(response, textStatus){
+      console.log(response);
+    },
+    error: function(request, textStatus, errorMessage){
+      console.log(errorMessage);
+    }
+  })
+}
+
+
+$(document).on('change', '.markComplete', function(){
+  if(this.checked){
+    markTaskAsComplete($(this).data('id'));
+  } else {
+    markTaskActive($(this).data('id'));
+  };
+});
 
 getAndDisplayAllTasks();
 
